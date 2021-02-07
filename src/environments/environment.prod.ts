967259a6e1954bb5c2c51e2e67fedb534ed14574
Thama Nedzungani <tNedzungani@JUTA.CO.ZA>
2021-02-07 20:06:59 +0200

    fix environment errors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+
+  // Auth client configurations
+  authServer: 'http://localhost:5000',
+  clientId : 'globalStoreWeb',
+  scopes: 'globalApi read write openid',
+
+  // Mapbox
+  mapboxKey: 'pk.eyJ1IjoibnR2aW5uIiwiYSI6ImNra2xzd2hsYjJrYWIybm1ucGRjeWg2OHcifQ.LJ4xUzbYzprvzVlXf0fScg',
+
+  // GlobalStoreAPi Endpoints
+  globalStoreCountries: 'http://localhost:5001/v1/Countries' 
 };
