d4b28eb4b44951877dcc81d036b1a98c7be4f2eb
Thama Nedzungani <tNedzungani@JUTA.CO.ZA>
2021-02-07 19:48:23 +0200

    add mapbox feature

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-map',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+  map: mapboxgl.Map;
+
   constructor() { }
 
   ngOnInit(): void {
-  }
+    mapboxgl.accessToken = environment.mapboxKey;
+    this.map = new mapboxgl.Map({
+      container: 'global-map',
+      center: [-74.5, 40],
+      zoom: 9,
+      style: 'mapbox://styles/mapbox/streets-v11'
+    });
 
+    this.map.addControl(new mapboxgl.GeolocateControl({
+      positionOptions: {
+      enableHighAccuracy: true
+      },
+      trackUserLocation: true
+      }), "top-left");
+
+      this.map.addControl(new mapboxgl.NavigationControl(), "top-left");
+  }
 }
